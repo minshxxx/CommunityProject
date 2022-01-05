669e39456c040dd0681d8fa89479a0db6fc66516
Ubuntu <ubuntu@ip-172-31-47-215.ap-northeast-2.compute.internal>
2022-01-06 00:18:02 +0900

    자동화

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const app = require('./app.js');
+const schedule = require('./schedule')
+
 const port = 3000;
+
+schedule();
 
 app.listen( port, function(){
     console.log('Express listening on port', port);
