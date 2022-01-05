35790872cca8d618d454dd8fe246fba78bc53a95
minshxxx <minshxxx@gmail.com>
2022-01-05 15:35:01 +0900

    db.sequelize.sync() app.js 에만 등록

--- database.js
+++ database.js
@@ -1,25 +1,6 @@
 const db = require('./models')
 
 const inputData = (siteData) => {
-    console.log(`inputData 함수 호출`)
-    db.sequelize.authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-        db.sequelize.sync();
-    })
-    .then(() => {
-        console.log('DB Sync complete.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
-    
-    db.Sites.destroy({
-        where: {
-            site: siteData.site,
-        }
-    })
-
     db.Sites.create({
         site: siteData.site,
         subject : siteData.subject,
