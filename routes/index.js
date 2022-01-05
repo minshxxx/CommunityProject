37b95a6c5a1b14dd8dc786d6f317536601fbedfa
Ubuntu <ubuntu@ip-172-31-47-215.ap-northeast-2.compute.internal>
2022-01-06 00:01:37 +0900

    date 내림차순으로 data 불러오기

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,12 @@ var express = require('express');
 var models = require('../models')
 var router = express.Router();
 
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   let data, data2, data3, data4, data5;
-//   let func = async () => { 
-//     data = await require('./../todayhumor').getData2()
-//     // data2 = await require('../dogdrip').getData2()
-//     data3 = await require('../fmkorea').getData2()
-//     data4 = await require('../ppomppu').getData2()
-//     data5 = await require('../humoruniv').getData2()
-
-//     res.render('index', { title: 'Express', data, data3, data4, data5 })
-//   } 
-//   func();
-// })
-
 router.get('/', async (req, res, next) => {
-  await models.Sites.findAll()
+  await models.Sites.findAll({
+    order: [
+      ['date', 'DESC']
+    ]
+  })
   .then( (data) => {
       res.render('index', { data })
   })
