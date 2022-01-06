ecf9e57b25176ac7b9acb37e93de44108ee4e53f
minshxxx <minshxxx@gmail.com>
2022-01-06 12:13:42 +0900

    수정

--- schedule.js
+++ schedule.js
@@ -5,8 +5,12 @@ const humoruniv = require('./sites/humoruniv')
 const ppomppu = require('./sites/ppomppu')
 const todayhumor = require('./sites/todayhumor')
 
-module.exports = () => {
-    schedule.scheduleJob('0 0 0/15 * * *', async () => {
+module.exports = async() => {
+    await fmkorea.getData2();
+    await humoruniv.getData2();
+    await ppomppu.getData2();
+    await todayhumor.getData2();
+    schedule.scheduleJob('0 0/15 * * * *', async () => {
         await fmkorea.getData2();
         await humoruniv.getData2();
         await ppomppu.getData2();
